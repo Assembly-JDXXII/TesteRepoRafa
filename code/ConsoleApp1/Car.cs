670ff406f0b4df7af3f078ed962723ca1019aeba
RafaelCosta <RafaelCosta@PT-NEW-R6>
2025-01-18 11:53:36 +0000

    Added first exercises code II

---
+++ code/ConsoleApp1/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Car
+    {
+        public string color;
+
+        public Brand brand;
+
+        public int fuel;
+        public int distance;
+        public int fuelEfficiency; //L/100km
+
+        public Car() { }
+        public Car(string color)
+        {
+            this.color = color;
+        }
+        
+        public void GoDistance(int distance)
+        {
+            fuel -= (int)(fuelEfficiency*(distance/100.0));
+            this.distance += distance;
+        }
+        
+    }
+}
--- code/ConsoleApp1/Program.cs
+++ code/ConsoleApp1/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Pintas e tintas");
-    
+﻿using ConsoleApp1;
+
+
+Car car1 = new Car("Red");
+
+Console.WriteLine("Distance: " + car1.distance);
+
+car1.fuel = 10;
+car1.fuelEfficiency = 8;
+car1.brand = new Brand();
+
+car1.GoDistance(50);
+
+Console.WriteLine("Distance: "+car1.distance);
+Console.WriteLine("fuel: "+car1.fuel);
